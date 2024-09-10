da858a716058449c7cb86e94617103137de4b041
kevin <1026838160@qq.com>
2024-09-10 15:24:12 +0800

    feat: update readme & use cdn import pdfjs

--- app/_components/Content/index.tsx
+++ app/_components/Content/index.tsx
@@ -13,10 +13,7 @@ import Tools from "./Tools";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.mjs",
-  import.meta.url
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 const options = {
   cMapUrl: "/cmaps/",
